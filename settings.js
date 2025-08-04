86a6514793922597c564eb41d0346978e21caa16
Techwizard18 <stephwafula99@gmail.com>
2025-08-04 12:50:13 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '𝚂𝙽𝙾𝚆𝙱𝙸𝚁𝙳',
   botName: "𝗟𝗮𝗱𝘆_𝗕𝗲𝗹𝗹𝗮🎀",
   botOwner: 'Vosty', // Your name
-  ownerNumber: '254117812436', //Set your number here without + symbol.
+  ownerNumber: '254762414548', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
